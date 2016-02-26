ac6ce9e89f56ccb5dfecab52393c673d05fcd975
Chander G   - Dual 2012 <12CS30011@cpusrv-gpu-nvidia.(none)>
2016-02-26 21:23:29 +0530

    obtain args from user
    obtain all required values and filename from user

--- main.py
+++ main.py
@@ -13,9 +13,52 @@ Assumptions:
   1. Only backface culling
 """
 
-def main():
-  """ Main runner. """
+import optparse
+
+def convertToImage(params, filename):
+  """ Convert obj object to svg image. 
+  
+  Top level worker for business logic.
+
+  INPUT
+  -----
+  params -- hash of params
+  filename -- name of input obj file
+  """
   print "WIP"
+
+def main():
+  """ Main runner. 
+  
+  Just a wrapper on main worker to take in args etc.
+  """
+  usage = "usage: %prog [options] <name of obj file>"
+  parser = optparse.OptionParser(usage=usage)
+
+  parser.add_option("-x", "--vx", type="int", dest="vx", default="0", help="viewer x coordinate")
+  parser.add_option("-y", "--vy", type="int", dest="vy", default="0", help="viewer y coordinate")
+  parser.add_option("-z", "--vz", type="int", dest="vz", default="0", help="viewer z coordinate")
+
+  parser.add_option("-H", "--vh", type="int", dest="vh", default="100", help="viewport height")
+  parser.add_option("-W", "--vw", type="int", dest="vw", default="100", help="viewport width")
+
+  (options, args) = parser.parse_args()
+
+  if len(args) != 1:
+	parser.error("incorrect number of arguments")
+
+  params = {}
+  params['vx'] = options.vx
+  params['vy'] = options.vy
+  params['vz'] = options.vz
+  params['H'] = options.vh
+  params['W'] = options.vw
+
+  print "Using values: "
+  print params
+
+  # pass on to main worker
+  convertToImage(params, args[0])
 
 if __name__ == "__main__":
   main()
