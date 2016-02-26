d1304602fc04635ffb616bcc87012af4cabcffcb
ChanderG <chandergovind@gmail.com>
2016-02-26 22:23:13 +0530

    parse out object from obj file
    stores in a custom structure in a dict

--- main.py
+++ main.py
@@ -15,6 +15,54 @@ Assumptions:
 
 import optparse
 
+def loadObjectFromObj(filename):
+  """ Load object from obj file into custom structure.
+
+  INPUT
+  -----
+  filename -- name of obj file
+
+  OUPUT
+  -----
+  dict object containing vertices and faces
+  """
+  obj = {}
+  obj['vertices'] = {}
+  obj['faces'] = []
+
+  # raw text from obj
+  obj_raw = open(filename).read().strip('\n')
+  
+  # content lines
+  obj_raw_lines = filter(lambda x: x!="", obj_raw.split('\n'))
+
+  vertex_count = 1
+
+  for line in obj_raw_lines:
+    l = filter(lambda x: x!= '', line.split(' '))
+    # handle faces
+    if l[0] == 'f':
+	    if len(l) == 5:
+	      obj['faces'].append((int(l[1]), int(l[2]), int(l[3]), int(l[4])))
+	    elif len(l) == 4:
+	      obj['faces'].append((int(l[1]), int(l[2]), int(l[3])))
+	    else:
+	      print "Illegal condition"
+	      exit(1)
+    # handle vertices
+    elif l[0] == 'v':
+      if len(l) == 4:
+        obj['vertices'][vertex_count] = (float(l[1]), float(l[2]), float(l[3]))
+        vertex_count += 1
+      else:
+				print "Illegal condition"
+				exit(1)
+    else:
+      print "Illegal condition"
+      exit(1)
+
+  return obj
+
 def convertToImage(params, filename):
   """ Convert obj object to svg image. 
   
@@ -25,7 +73,10 @@ def convertToImage(params, filename):
   params -- hash of params
   filename -- name of input obj file
   """
-  print "WIP"
+
+  # load object from obj file
+  obj = loadObjectFromObj(filename)
+  print obj
 
 def main():
   """ Main runner. 
@@ -45,7 +96,7 @@ def main():
   (options, args) = parser.parse_args()
 
   if len(args) != 1:
-	parser.error("incorrect number of arguments")
+    parser.error("incorrect number of arguments")
 
   params = {}
   params['vx'] = options.vx
