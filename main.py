5f214a80c3d9a436065522f4183537e304a78af2
Chander G   - Dual 2012 <12CS30011@cpusrv-gpu-nvidia.(none)>
2016-02-26 20:53:51 +0530

    init commit
    specs and greenlight for python as implementation lang

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Create a 2d svg image from a 3d object.
+
+User input:
+  1. Viewpoint
+  2. Input obj file.
+  3. Dimensions of viewport
+
+Output:
+  1. svg file
+
+Assumptions:
+  1. Only backface culling
+"""
+
+def main():
+  """ Main runner. """
+  print "WIP"
+
+if __name__ == "__main__":
+  main()
