730b530cd24c3191a9593f87042279886d7136d4
ChanderG <chandergovind@gmail.com>
2016-02-26 23:07:42 +0530

    draw svg from image structure
    as it is created, it is too small to be visible -- use dummy scale up to
    make object visible; also clean up the indentation in  initial parts;
    overall cube looks good; f16 fails with some other error;
    scaling/rotating/fitting to viewport etc to be done

--- main.py
+++ main.py
@@ -16,52 +16,55 @@ Assumptions:
 import optparse
 
 def loadObjectFromObj(filename):
-  """ Load object from obj file into custom structure.
+    """ Load object from obj file into custom structure.
 
-  INPUT
-  -----
-  filename -- name of obj file
+    INPUT
+    -----
+    filename -- name of obj file
 
-  OUPUT
-  -----
-  dict object containing vertices and faces
-  """
-  obj = {}
-  obj['vertices'] = {}
-  obj['faces'] = []
+    OUPUT
+    -----
+    dict object containing vertices and faces
+    """
+    obj = {}
+    obj['vertices'] = {}
+    obj['faces'] = []
 
-  # raw text from obj
-  obj_raw = open(filename).read().strip('\n')
-  
-  # content lines
-  obj_raw_lines = filter(lambda x: x!="", obj_raw.split('\n'))
+    # raw text from obj
+    obj_raw = open(filename).read().strip('\n')   
 
-  vertex_count = 1
+    # content lines
+    obj_raw_lines = filter(lambda x: x!="", obj_raw.split('\n'))
 
-  for line in obj_raw_lines:
-    l = filter(lambda x: x!= '', line.split(' '))
-    # handle faces
-    if l[0] == 'f':
-      if len(l) == 5:
-        obj['faces'].append((int(l[1]), int(l[2]), int(l[3]), int(l[4])))
-      elif len(l) == 4:
-        obj['faces'].append((int(l[1]), int(l[2]), int(l[3])))
-      else:
-        print "Illegal condition"
-        exit(1)
-    # handle vertices
-    elif l[0] == 'v':
-      if len(l) == 4:
-        obj['vertices'][vertex_count] = (float(l[1]), float(l[2]), float(l[3]))
-        vertex_count += 1
-      else:
-        print "Illegal condition"
-        exit(1)
-    else:
-      print "Illegal condition"
-      exit(1)
+    vertex_count = 1
 
-  return obj
+    for line in obj_raw_lines:
+        if line.strip(' ')[0] == '#':
+            continue
+        l = filter(lambda x: x!= '', line.split(' '))
+        # handle faces
+        if l[0] == 'f':
+            if len(l) == 5:
+                obj['faces'].append((int(l[1]), int(l[2]), int(l[3]), int(l[4])))
+            elif len(l) == 4:
+                obj['faces'].append((int(l[1]), int(l[2]), int(l[3])))
+            else:
+                print "Illegal condition: Too many vertices in face."
+                exit(1)
+        # handle vertices
+        elif l[0] == 'v':
+            if len(l) == 4:
+                obj['vertices'][vertex_count] = (float(l[1]), float(l[2]), float(l[3]))
+                vertex_count += 1
+            else:
+                print "Illegal condition: Too many coordinates for a vertex."
+                exit(1)
+        else:
+            print l
+            print "Illegal condition: "
+            exit(1)
+
+    return obj
 
 def calculateCentroid(face, vertices):
     """
@@ -215,6 +218,46 @@ def project2D(obj, params):
 
     return img
 
+def drawSvg(img, filename):
+    """ Draw svg image from img structure.
+
+    img -- struct of image data
+    filename -- output file name
+
+    Creates a svg file called -- filename.svg containing required image.
+    """
+    vertices = img["vertices"]
+
+    f = open(filename, 'w')
+    f.write('<svg xmlns="http://www.w3.org/2000/svg" version="1.1">')
+    f.write('\n')
+
+    for face in img["faces"]:
+        # format the point string
+        if len(face) == 3:
+            a, b, c = face
+            vertex_str = "{0},{1} {2},{3} {4},{5}".format(vertices[a][0], vertices[a][1], vertices[b][0], vertices[b][1], vertices[c][0], vertices[c][1])
+        if len(face) == 4:
+            a, b, c , d = face
+            vertex_str = "{0},{1} {2},{3} {4},{5} {6},{7}".format(vertices[a][0], vertices[a][1], vertices[b][0], vertices[b][1], vertices[c][0], vertices[c][1], vertices[d][0], vertices[d][1])
+        # draw polygon
+        f.write('<polygon points="{0}" style="fill:red;stroke:black;stroke-width:1" />'.format(vertex_str))
+        f.write('\n')
+    f.write('</svg>')
+    f.close()
+
+def dummyScaleUp(img):
+    """ Dummy placeholder scaleup for test purpose. """
+
+    orig_vert = img["vertices"]
+    new_vert = {}
+    for v in orig_vert.keys():
+        x, y, z =  orig_vert[v]
+        v_ = int(100*x), int(100*y), int(100*z)
+        new_vert[v] = v_
+    img["vertices"] = new_vert
+    return img
+
 def convertToImage(params, filename):
   """ Convert obj object to svg image. 
   
@@ -229,11 +272,26 @@ def convertToImage(params, filename):
   # load object from obj file
   obj = loadObjectFromObj(filename)
 
+  # object roations etc here
+
+  # back face culling
   print "Back face culling..."
   obj = backFaceCull(obj, params)
 
+  # move back object to negative z here
+
   # project to 2d in the right order
+  print "Projecting to 2d..."
   img = project2D(obj, params)
+
+  # scale up/down etc to fit in viewport here
+
+  # dummy scale up to make stuff visible
+  img = dummyScaleUp(img)
+
+  # create svg from 2d image
+  print "Creating svg image: {0}".format("output.svg")
+  drawSvg(img, 'output.svg')
 
 def main():
   """ Main runner. 
